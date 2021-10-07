b736c7f268de281d7fca4ba3d880b773c0ee0938
jswebguru <jswebguru77@gmail.com>
2021-10-07 12:11:24 +0100

    Update the box url

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from flask import Flask, render_template, request
-import requests
+
 
 app = Flask(__name__)
-
-# result = []
-
-
-@app.route("/")
-def hello():
-    return render_template('home.html')
-
-
-@app.route("/api/get_data/", methods=['POST'])
-def get_result():
-    result = '123'
-    return result
 
 
 @app.route("/box/", methods=['POST'])
@@ -23,8 +10,6 @@ def get_meta_data():
     file_id = request.form['file_ids']
 
     print(f'user_id is {user_id} and file id is {file_id}')
-
-    result = {'file_id': file_id, 'user_id': user_id}
 
     return render_template('home.html', user_id=user_id, file_id=file_id)
 
