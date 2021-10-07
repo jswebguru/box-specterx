e026b4c778075b0a2b7ce29e37a0d305170f34a8
jswebguru <jswebguru77@gmail.com>
2021-10-07 19:27:16 +0100

    Get file metadata

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-
+from boxsdk import OAuth2, Client
+from settings import CLIENT_ID, CLIENT_SECRET, DEVELOPER_TOKEN
 
 app = Flask(__name__)
 
@@ -7,12 +8,51 @@ app = Flask(__name__)
 @app.route("/box/", methods=['POST'])
 def get_meta_data():
     user_id = request.form['user_id']
-    file_id = request.form['file_ids']
+    user_name = request.form['user_name']
+    file_id = request.form['file_id']
 
-    print(f'user_id is {user_id} and file id is {file_id}')
+    auth = OAuth2(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        access_token=DEVELOPER_TOKEN,
+    )
+    client = Client(auth)
 
-    return render_template('home.html', user_id=user_id, file_id=file_id)
+    file_info = client.file(file_id).get()
+    if file_info:
+        file_name = file_info.name
+        file_size = file_info.size
+        last_modification_time = file_info.modified_at
+        file_owner_name = file_info.owned_by.name
+        # user_name = client.user().get().name
+
+        return render_template(
+            'home.html',
+            user_name=user_name,
+            file_name=file_name,
+            file_size=file_size,
+            file_owner_name=file_owner_name,
+            last_modification_time=last_modification_time,
+        )
+
+    # folder_info = client.folder(folder_id=file_id).get()
+    # if folder_info:
+    #     total_count = folder_info.item_collection.total_count
+    #     folder_file_lists = []
+    #     for index in range(0, total_count):
+    #         folder_file_info = folder_info.item_collection.entries[index]
+    #         folder_file_id = folder_file_info.id
+    #         folder_file_name = folder_file_info.name
+    #         folder_file_lists.append({'folder_file_id': folder_file_id, 'folder_file_name': folder_file_name})
+    #
+    #     return folder_file_lists
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0')
+    # print(f'user_id is {user_id} and file id is {file_id}')
+
+    # return render_template('home.html', user_id=user_id, file_id=file_id)
+    # return render_template('home.html', file_id=file_id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
