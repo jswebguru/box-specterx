ad5f79a6e1930760383fccad0e15bead65abd4bf
jswebguru <jswebguru77@gmail.com>
2021-10-07 22:50:00 +0100

    Add folder download

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from boxsdk import OAuth2, Client, BoxAPIException
 from settings import CLIENT_ID, CLIENT_SECRET, DEVELOPER_TOKEN
@@ -38,18 +39,24 @@ def get_meta_data():
         status = e.status
         if status == 404:
             folder_info = client.folder(folder_id=file_id).get()
+
             data_type = folder_info.type
             folder_owner = folder_info.created_by.name
             folder_name = folder_info.name
             folder_size = folder_info.size
             folder_modified_at = folder_info.modified_at
-            total_count = folder_info.item_collection['total_count']
+            file_lists = folder_info.item_collection['entries']
             folder_file_lists = []
-            for index in range(0, total_count):
-                folder_file_info = folder_info.item_collection['entries'][index]
-                folder_file_id = folder_file_info.id
-                folder_file_name = folder_file_info.name
-                folder_file_lists.append({'folder_file_id': folder_file_id, 'folder_file_name': folder_file_name})
+
+            for file_data in file_lists:
+                folder_file_id = file_data.id
+                folder_file_name = file_data.name
+                file_download_url = client.file(folder_file_id).get_download_url()
+                folder_file_lists.append({
+                    'folder_file_id': folder_file_id,
+                    'folder_file_name': folder_file_name,
+                    'file_download_url': file_download_url
+                })
 
             return render_template(
                 'home.html',
@@ -64,20 +71,36 @@ def get_meta_data():
         print(e)
 
 
-@app.route('/api/file_download/', methods=['POST'])
+@app.route('/api/folder_download/', methods=['POST'])
 def file_download():
-    file_id = request.form['file_id']
+    folder_id = request.form['folder_id']
 
     auth = OAuth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=DEVELOPER_TOKEN)
     client = Client(auth)
+    folder_info = client.folder(folder_id=folder_id).get()
+    file_lists = folder_info.item_collection['entries']
+    folder_name = folder_info.name
 
-    file_content = client.file(file_id).content()
-    print(file_content)
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    download_file_folder = os.path.join(current_path, folder_name)
+    if not os.path.exists(download_file_folder):
+        os.mkdir(download_file_folder)
+
+    for file in file_lists:
+        file_id = file.id
+        file_name = file.name
+
+        file_content = client.file(file_id).content()
+        download_file_name = os.path.join(download_file_folder, file_name)
+
+        with open(download_file_name, 'wb') as f:
+            f.write(file_content)
+            f.close()
 
     result = 'Successfully Downloaded!'
 
     return result
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0')
